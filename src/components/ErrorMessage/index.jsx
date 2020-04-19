470195b9d2c2f005447324c5dda488934cd424ff
Luke Murphey <Luke.Murphey@gmail.com>
2020-04-18 18:58:01 -0700

    Adding error messaging

---
+++ src/components/ErrorMessage/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Header, Segment } from 'semantic-ui-react';
+
+/**
+ * This class renders the content of chapter of a work.
+ */
+function ErrorMessage({ title, description, message }) {
+  return (
+    <Segment color="red">
+      <Header as="h3">{title}</Header>
+      {description && (
+        description
+      )}
+      {message && description && (
+        <div style={{ marginBottom: 12 }} />
+      )}
+      { message }
+    </Segment>
+  );
+}
+
+ErrorMessage.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  message: PropTypes.string.isRequired,
+};
+
+ErrorMessage.defaultProps = {
+  description: null,
+};
+
+export default ErrorMessage;
--- src/components/Reader/index.jsx
+++ src/components/Reader/index.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import {
-  Button, Input, Icon, Dropdown, Dimmer, Loader, Container, Header, Grid,
+  Button, Input, Icon, Dropdown, Container, Header, Grid, Placeholder,
 } from 'semantic-ui-react';
 import { ENDPOINT_READ_WORK } from '../Endpoints';
 import Chapter from './Chapter';
+import ErrorMessage from '../ErrorMessage';
 import './index.css';
 
 class Reader extends Component {
+  static convertDivisionsToOptions(divisions) {
+    return divisions.map((d) => ({
+      key: d.description,
+      text: d.full_title,
+      value: d.title_slug,
+    }));
+  }
+
+  /**
+   * This function looks for the chapter the user entered. It looks both in the description as well
+   * as the key.
+   * This is useful because the works often use a Greek name as the description but an English
+   * description too and we want both to be searchable (don't want users to have to type Μαρκον).
+   * @param {*} options List of dropdown options
+   * @param {*} query The string that the user entered that we are searching for
+   */
+  static workSearch(options, query) {
+    return options.filter((opt) => opt.text.includes(query) || opt.key.includes(query));
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       modal: null,
       data: null,
+      error: null,
     };
   }
 
@@ -21,7 +43,11 @@ class Reader extends Component {
       .then((data) => {
         this.setState({ data });
       })
-      .catch(console.log);
+      .catch((e) => {
+        this.setState({
+          error: e.toString(),
+        });
+      });
   }
 
   closeModal() {
@@ -33,7 +59,7 @@ class Reader extends Component {
   }
 
   render() {
-    const { modal, data } = this.state;
+    const { modal, data, error } = this.state;
     return (
       <div>
         <Input
@@ -82,7 +108,26 @@ class Reader extends Component {
             <Grid>
               <Grid.Row>
                 <Grid.Column width={8}>
-                  <Header floated="left" as="h3">{data.chapter.full_title}</Header>
+                  <Header floated="left" as="h3">
+                    {data.chapter.parent_division && (
+                      <>
+                        <Dropdown
+                          inline
+                          floating
+                          deburr
+                          scrolling
+                          search={Reader.workSearch}
+                          options={Reader.convertDivisionsToOptions(data.divisions)}
+                          defaultValue={data.chapter.parent_division.title_slug}
+                          style={{ minWidth: 100 }}
+                        />
+                        <div style={{ display: 'inline-block', paddingLeft: 6 }}>
+                          {data.chapter.type}
+                          {data.chapter.descriptor}
+                        </div>
+                      </>
+                    )}
+                  </Header>
                 </Grid.Column>
                 <Grid.Column width={8}>
                   <Container textAlign="right">
@@ -95,10 +140,33 @@ class Reader extends Component {
             <Chapter chapter={data.chapter} content={data.content} work={data.work} />
           </>
         )}
-        {!data && (
-          <Dimmer active inverted>
-            <Loader inverted>Loading</Loader>
-          </Dimmer>
+        {error && (
+          <ErrorMessage title="Unable to load the content" description="The given chapter could not be loaded from the server" message={error} />
+        )}
+        {!data && !error && (
+          <Placeholder style={{ marginTop: 32 }}>
+            <Placeholder.Header>
+              <Placeholder.Line />
+            </Placeholder.Header>
+            <Placeholder.Paragraph>
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder.Paragraph>
+            <Placeholder.Paragraph>
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder.Paragraph>
+            <Placeholder.Paragraph>
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder.Paragraph>
+          </Placeholder>
         )}
 
       </div>
