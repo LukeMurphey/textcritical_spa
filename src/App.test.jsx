28a87f04925f052c8b7bd28762ee0aae78947284
Luke Murphey <Luke.Murphey@gmail.com>
2020-04-19 22:32:28 -0700

    Making tests work

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,8 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('App loads', () => {
+  // https://stackoverflow.com/questions/36069731/how-to-unit-test-api-calls-with-mocked-fetch-in-react-native-with-jest
+  global.fetch = jest.fn(() => Promise.resolve());
   const { getByText } = render(<App />);
   expect(getByText(/About/i)).toBeTruthy();
 });
