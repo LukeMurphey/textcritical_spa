5cd0e0701a70d3b1c330a39aea11692d3de133c3
Luke Murphey <Luke.Murphey@gmail.com>
2020-04-29 23:19:31 -0700

    Adding basic app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter, Route } from 'react-router-dom';
 import { Menu, Container } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import AboutDialog from './components/AboutDialog/index';
@@ -20,7 +21,7 @@ class App extends Component {
 
   render() {
     const { modal } = this.state;
-    const { inverted, includeMenu } = this.props;
+    const { inverted, includeMenu, match, location, history } = this.props;
 
     return (
       <div>
@@ -64,14 +65,68 @@ class App extends Component {
   }
 }
 
+/*
+const App = () => (
+  <div>
+    { includeMenu && (
+      <Menu inverted={!inverted} attached="top">
+        <Container>
+          <Menu.Item style={{ backgroundColor: '#009ec2' }}>
+            <img alt="" style={{ width: 22, height: 22 }} src="book.png" />
+          </Menu.Item>
+          <Menu.Item
+            name="about"
+            active={false}
+            content="About"
+            onClick={() => this.openAboutModal()}
+          />
+
+          <Menu.Menu position="right">
+            <div className="ui right aligned category search item">
+              <div className="ui transparent icon input">
+                <input
+                  className="prompt"
+                  type="text"
+                  placeholder="Search works..."
+                />
+                <i className="search link icon" />
+              </div>
+              <div className="results" />
+            </div>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    )}
+    {modal === 'about' && (
+      <AboutDialog onClose={() => this.closeModal()} />
+    )}
+    <Container style={{ marginTop: 12 }}>
+      <Router>
+        <Switch>
+          <Route path="work/:work">
+            {App.getReader(inverted)}
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
+  </div>
+);
+*/
+
 App.propTypes = {
   inverted: PropTypes.bool,
   includeMenu: PropTypes.bool,
+  match: PropTypes.object,
+  location: PropTypes.object,
+  history: PropTypes.object,
 };
 
 App.defaultProps = {
   inverted: false,
   includeMenu: false,
+  match: null,
+  location: null,
+  history: null,
 };
 
-export default App;
+export default withRouter(App);
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render((<Router history={history}><Route path="/work/:work"><App /></Route></Router>), document.getElementById('root'));
