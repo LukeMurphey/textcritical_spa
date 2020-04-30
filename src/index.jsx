27b71a2b4cfb21420d8f75d07e33a58813cce7b6
Luke Murphey <Luke.Murphey@gmail.com>
2020-04-29 23:24:45 -0700

    Improving routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import App from './App';
 
 const history = createBrowserHistory();
 
-ReactDOM.render((<Router history={history}><Route path="/work/:work"><App /></Route></Router>), document.getElementById('root'));
+ReactDOM.render((
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/work/:work">
+        <App />
+      </Route>
+      <Route exact path="/work/">
+        <App />
+      </Route>
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>), document.getElementById('root'));
