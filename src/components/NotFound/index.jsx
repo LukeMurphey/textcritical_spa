87e5336d61e6ae3f77a067ca561d2a87aa6a4033
Luke Murphey <Luke.Murphey@gmail.com>
2020-05-02 18:57:42 -0700

    Adding routing to a not found page

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h3>No match</h3>
+  </div>
+);
+
+export default NotFound;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import NotFound from './components/NotFound';
 import App from './App';
 
 const history = createBrowserHistory();
@@ -9,6 +10,25 @@ const history = createBrowserHistory();
 ReactDOM.render((
   <Router history={history}>
     <Switch>
+      <Route exact path="/work/:work?/:division0?/:division1?/:division2?/:division3?/:division4?/:leftovers?">
+        <App />
+      </Route>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  </Router>), document.getElementById('root'));
+
+/*
+ReactDOM.render((
+  <Router history={history}>
+    <Switch>
+      <Route path="/work">
+        <App />
+      </Route>
       <Route path="/work/:work?/:division0?/:division1?/:division2?/:division3?/:division4?/:leftovers?">
         <App />
       </Route>
@@ -17,3 +37,5 @@ ReactDOM.render((
       </Route>
     </Switch>
   </Router>), document.getElementById('root'));
+*/
+
