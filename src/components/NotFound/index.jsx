d4c6b4fcfa8c795977818f556d2ff9c0bd2a24af
Luke Murphey <Luke.Murphey@gmail.com>
2020-05-02 20:38:33 -0700

    Improving the page for bad links

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+  Header, Segment, Container, Button,
+} from 'semantic-ui-react';
 
 const NotFound = () => (
-  <div>
-    <h3>No match</h3>
-  </div>
+  <Container style={{ marginTop: 32 }}>
+    <Segment color="red">
+      <Header as="h1">Page Not Found</Header>
+      I cannot seem to find the page you are looking for.
+      Your best bet is probably just to head up to the
+      {' '}
+      <a href="/">homepage</a>
+      <div style={{ marginTop: 16 }}>
+        <Button onClick={() => { window.location = '/'; }}>Take me to the homepage</Button>
+      </div>
+    </Segment>
+  </Container>
 );
 
 export default NotFound;
