6b4ab48ec869db3d6ba5bee82714431e43270735
Luke Murphey <Luke.Murphey@gmail.com>
2020-05-03 21:02:37 -0700

    Breaking out the PopupDialog

---
+++ src/components/PopupDialog/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Button, Segment, Icon } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+const PopupDialog = (props) => {
+  const {
+    children, onClose, x, y, positionBelow, positionRight,
+  } = props;
+
+  /**
+   * This is done to get rid of the outline around the close button and get it to show up at the
+   * right of the screen.
+   */
+  const closeButtonStyle = {
+    float: 'right',
+    WebkitBoxShadow: '0 0 0 0',
+    boxShadow: '0 0 0 0',
+  };
+
+  // Correct for the cases where the dialog is off of the bottom of the screen.
+  const segmentStyle = {
+    position: 'absolute',
+    width: 500,
+    height: 300,
+    overflowY: 'auto',
+  };
+
+  if (positionBelow) {
+    segmentStyle.top = y;
+  } else {
+    segmentStyle.top = y - segmentStyle.height - 20;
+  }
+
+  if (positionRight) {
+    segmentStyle.left = x;
+  } else {
+    segmentStyle.left = x - segmentStyle.width - 10;
+  }
+
+  return (
+    <Segment style={segmentStyle}>
+      <Button basic style={closeButtonStyle} icon onClick={onClose}>
+        <Icon name="close" />
+      </Button>
+      {children}
+    </Segment>
+  );
+};
+
+PopupDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  positionBelow: PropTypes.bool,
+  positionRight: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+};
+
+PopupDialog.defaultProps = {
+  positionBelow: true,
+  positionRight: true,
+};
+
+export default PopupDialog;
--- src/components/WordInformation/WordInformationPopup.jsx
+++ src/components/WordInformation/WordInformationPopup.jsx
@@ -1,50 +1,23 @@
 import React from 'react';
-import { Button, Segment, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import WordInformation from '.';
+import PopupDialog from '../PopupDialog';
 
 const WordInformationDialog = (props) => {
   const {
     word, onClose, x, y, positionBelow, positionRight,
   } = props;
 
-  /**
-   * This is done to get rid of the outline around the close button and get it to show up at the
-   * right of the screen.
-   */
-  const closeButtonStyle = {
-    float: 'right',
-    WebkitBoxShadow: '0 0 0 0',
-    boxShadow: '0 0 0 0',
-  };
-
-  // Correct for the cases where the dialog is off of the bottom of the screen.
-  const segmentStyle = {
-    position: 'absolute',
-    width: 500,
-    height: 300,
-    overflowY: 'auto',
-  };
-
-  if (positionBelow) {
-    segmentStyle.top = y;
-  } else {
-    segmentStyle.top = y - segmentStyle.height - 20;
-  }
-
-  if (positionRight) {
-    segmentStyle.left = x;
-  } else {
-    segmentStyle.left = x - segmentStyle.width - 10;
-  }
-
   return (
-    <Segment style={segmentStyle}>
-      <Button basic style={closeButtonStyle} icon onClick={onClose}>
-        <Icon name="close" />
-      </Button>
+    <PopupDialog
+      onClose={onClose}
+      x={x}
+      y={y}
+      positionBelow={positionBelow}
+      positionRight={positionRight}
+    >
       <WordInformation word={word} />
-    </Segment>
+    </PopupDialog>
   );
 };
 
