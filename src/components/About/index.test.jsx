a9bb03218273369545b2da14c4446b515d5539d1
Luke Murphey <Luke.Murphey@gmail.com>
2020-06-03 22:30:19 -0700

    Fixing tests

--- src/components/About/index.test.jsx
+++ src/components/About/index.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import About from './index';
 
-test('toTitleCase', () => {
-  const { getByText } = render(<About />);
+test('About page renders', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
   expect(getByText(/About/i)).toBeTruthy();
 });
--- src/components/ContactMe/index.test.jsx
+++ src/components/ContactMe/index.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import ContactMe from './index';
 
 test('ContactMe', () => {
-  const { getByText } = render(<ContactMe />);
+  const { getByText } = render(
+    <MemoryRouter>
+      <ContactMe />
+    </MemoryRouter>,
+  );
   expect(getByText(/Getting in Touch/i)).toBeTruthy();
 });
