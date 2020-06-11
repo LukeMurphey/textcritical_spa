a6d66e6995cec288bde16d23c00d0a3cbd1f770e
Luke Murphey <Luke.Murphey@gmail.com>
2020-06-11 14:24:11 -0700

    More tests

--- src/components/Settings/index.test.js
+++ src/components/Settings/index.test.js
@@ -1,4 +1,4 @@
-import { setWorkProgress, getWorksLastRead } from "./index";
+import { setWorkProgress, getWorksLastRead, maxHistoryCount } from "./index";
 
 // Setup a local storage mock
 class LocalStorageMock {
@@ -53,4 +53,42 @@ test("setting work progress", () => {
   expect(records[1].workTitleSlug).toBe("berean-study-bible");
   expect(records[1].divisions).toEqual(["Genesis", "1"]);
   expect(records[1].divisionReference).toBe("Genesis 1");
+});
+
+test("only stores the correct number of entries", () => {
+  const storageMock = new LocalStorageMock();
+  // Add some works
+  setWorkProgress("new-testament", ["Luke", "15"], "Luke 15", storageMock);
+  setWorkProgress("asv", ["Luke", "15"], "Luke 15", storageMock);
+  setWorkProgress(
+    "berean-study-bible",
+    ["Genesis", "1"],
+    "Genesis 1",
+    storageMock
+  );
+  setWorkProgress("lxx", ["Genesis", "1"], "Genesis 1", storageMock);
+  setWorkProgress("acharnians", [], "1", storageMock);
+  setWorkProgress("abdicatus", [], "1", storageMock);
+  setWorkProgress("republic", [], "1", storageMock);
+  setWorkProgress("republic-english", [], "1", storageMock);
+
+  // Make sure it only stores the correct number of entries
+  expect(getWorksLastRead(storageMock)).toHaveLength(maxHistoryCount());
+});
+
+test("handles corrupted data", () => {
+  const storageMock = new LocalStorageMock();
+  storageMock.setItem("lastReadHistory", "[{");
+
+  expect(getWorksLastRead(storageMock)).toBe(null);
+});
+
+test("ignores invalid entries", () => {
+  const storageMock = new LocalStorageMock();
+  storageMock.setItem(
+    "lastReadHistory",
+    '[{"workTitleSlugBAD":"republic-english","divisions":[],"divisionReference":"1"},{"workTitleSlug":"republic","divisions":[],"divisionReference":"1"}]'
+  );
+
+  expect(getWorksLastRead(storageMock)).toHaveLength(1);
 });
