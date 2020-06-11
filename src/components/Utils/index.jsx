610d0660d34cf50d9e6013120d45a8ab533288d0
Luke Murphey <Luke.Murphey@gmail.com>
2020-06-11 10:27:34 -0700

    Adding truncate string function

--- src/components/Utils/index.jsx
+++ src/components/Utils/index.jsx
@@ -1,4 +1,7 @@
-// eslint-disable-next-line import/prefer-default-export
 export function toTitleCase(str) {
   return str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+}
+
+export function truncate(str, maxLength) {
+    return str.length > maxLength ? `${str.substring(0, (maxLength-3))}...` : str;
 }
