fa31de884c07a88110d9d35b491a342d71dd8de4
Luke Murphey <Luke.Murphey@gmail.com>
2020-08-05 22:08:36 -0700

    Cleaning up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Menu, Container } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import AboutDialog from './components/AboutDialog/index';
 import Reader from './components/Reader';
 
-const MODAL_ABOUT = 'about';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { modal: null };
-  }
-
-  closeModal() {
-    this.setState({ modal: null });
-  }
-
-  openAboutModal() {
-    this.setState({ modal: MODAL_ABOUT });
-  }
-
-  render() {
-    const { modal } = this.state;
-    const { inverted, includeMenu } = this.props;
-
-    return (
-      <>
-        { includeMenu && (
-          <Menu inverted={!inverted} attached="top">
-            <Container>
-              <Menu.Item style={{ backgroundColor: '#009ec2' }}>
-                <img alt="" style={{ width: 22, height: 22 }} src="book.png" />
-              </Menu.Item>
-              <Menu.Item
-                name="about"
-                active={false}
-                content="About"
-                onClick={() => this.openAboutModal()}
-              />
-
-              <Menu.Menu position="right">
-                <div className="ui right aligned category search item">
-                  <div className="ui transparent icon input">
-                    <input
-                      className="prompt"
-                      type="text"
-                      placeholder="Search works..."
-                    />
-                    <i className="search link icon" />
-                  </div>
-                  <div className="results" />
-                </div>
-              </Menu.Menu>
-            </Container>
-          </Menu>
-        )}
-        {modal === MODAL_ABOUT && (
-          <AboutDialog onClose={() => this.closeModal()} />
-        )}
-        <Reader
-          inverted={inverted}
-        />
-      </>
-    );
-  }
-}
+const App = ({ inverted }) => (
+  <Reader
+    inverted={inverted}
+  />
+)
 
 App.propTypes = {
   inverted: PropTypes.bool,
-  includeMenu: PropTypes.bool,
 };
 
 App.defaultProps = {
   inverted: false,
-  includeMenu: false,
 };
 
 export default withRouter(App);
--- src/components/BookSelection/index.test.jsx
+++ src/components/BookSelection/index.test.jsx
@@ -3,19 +3,49 @@ import { render } from '@testing-library/react';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import BookSelection from './index';
 
-test('BookSelection', () => {
-  enableFetchMocks();
-  fetch.mockResponseOnce(JSON.stringify([{
+const works = [
+  {
     title: 'New Testament',
     title_slug: 'new-testament',
     author: '',
     editor: '',
     language: 'Greek',
-  }]));
+  },
+  {
+    title: 'American Standard Version',
+    title_slug: 'asv',
+    author: '',
+    editor: '',
+    language: 'English',
+  },
+  {
+    title: 'LXX',
+    title_slug: 'lxx',
+    author: '',
+    editor: '',
+    language: 'Greek',
+  },
+  {
+    title: 'Republic',
+    title_slug: 'republic',
+    author: '',
+    editor: '',
+    language: 'English',
+  },
+];
 
-  const { getByText } = render(<BookSelection onSelectWork={() => {}} />);
+const relatedWorks = [{'title_slug': 'asv'}];
+
+test('BookSelection', () => {
+  enableFetchMocks();
+  fetch.mockResponseOnce(JSON.stringify(works));
+
+  const { getByText } = render(<BookSelection onSelectWork={() => {}} loadedWork='new-testament' relatedWorks={relatedWorks} />);
 
   process.nextTick(() => {
     expect(getByText(/New Testament/i)).toBeTruthy();
+    expect(getByText(/This work/i)).toBeTruthy();
+    expect(getByText(/American Standard Version/i)).toBeTruthy();
+    expect(getByText(/Related Work/i)).toBeTruthy();
   });
 });
