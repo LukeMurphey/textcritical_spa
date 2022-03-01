23668e4ddde5aa1fa54d71e6db6bd67492161b35
Luke Murphey <Luke.Murphey@gmail.com>
2022-03-01 12:59:03 -0800

    Converting to a functional

--- src/components/WorkDownloadDialog/index.jsx
+++ src/components/WorkDownloadDialog/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Button, Header, Modal, Radio, Form,
 } from 'semantic-ui-react';
@@ -8,64 +8,50 @@ import { ENDPOINT_WORK_DOWNLOAD } from '../Endpoints';
 const MOBI_FORMAT = 'mobi';
 const EPUB_FORMAT = 'epub';
 
-class WorkDownloadDialog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      format: EPUB_FORMAT,
-    };
-  }
+const WorkDownloadDialog = ({ onClose, work }) => {
+  const [format, setFormat] = useState(EPUB_FORMAT);
 
-  handleChange(event, data) {
-    this.setState({
-      format: data.value,
-    });
-  }
-
-  download() {
-    const { format } = this.state;
-    const { work } = this.props;
+  const download = () => {
     document.location = ENDPOINT_WORK_DOWNLOAD(work, format);
   }
 
-  render() {
-    const { onClose } = this.props;
-    const { format } = this.state;
-
-    return (
-      <Modal size="tiny" defaultOpen onClose={onClose} closeIcon>
-        <Header icon="info" content="Download Book" />
-        <Modal.Content>
-          <div stryle={{ marginBottom: 12 }}>Select which format you would like to download:</div>
-          <Form>
-            <Form.Field>
-              <Radio
-                label="ePUB Format (Recommended)"
-                name="radioGroup"
-                value={EPUB_FORMAT}
-                checked={format === EPUB_FORMAT}
-                onChange={(e, d) => this.handleChange(e, d)}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Radio
-                label="MOBI Format (Amazon Kindle)"
-                name="radioGroup"
-                value={MOBI_FORMAT}
-                checked={format === MOBI_FORMAT}
-                onChange={(e, d) => this.handleChange(e, d)}
-              />
-            </Form.Field>
-          </Form>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={onClose}>Close</Button>
-          <Button primary onClick={() => this.download()}>Download</Button>
-        </Modal.Actions>
-      </Modal>
-    );
+  const handleChange = (event, data) => {
+    setFormat(data.value);
   }
-}
+
+  return (
+    <Modal size="tiny" defaultOpen onClose={onClose} closeIcon>
+      <Header icon="info" content="Download Book" />
+      <Modal.Content>
+        <div stryle={{ marginBottom: 12 }}>Select which format you would like to download:</div>
+        <Form>
+          <Form.Field>
+            <Radio
+              label="ePUB Format (Recommended)"
+              name="radioGroup"
+              value={EPUB_FORMAT}
+              checked={format === EPUB_FORMAT}
+              onChange={(e, d) => handleChange(e, d)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Radio
+              label="MOBI Format (Amazon Kindle)"
+              name="radioGroup"
+              value={MOBI_FORMAT}
+              checked={format === MOBI_FORMAT}
+              onChange={(e, d) => handleChange(e, d)}
+            />
+          </Form.Field>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={onClose}>Close</Button>
+        <Button primary onClick={() => download()}>Download</Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
 WorkDownloadDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
