6c5b8ff47782814caaa84dc4843190e02ddfb221
Luke Murphey <Luke.Murphey@gmail.com>
2022-04-02 12:45:28 -0400

    Fixing test

--- src/components/AboutDialog/index.test.jsx
+++ src/components/AboutDialog/index.test.jsx
@@ -3,6 +3,8 @@ import { render } from '@testing-library/react';
 import AboutDialog from './index';
 
 test('toTitleCase', () => {
+  // https://stackoverflow.com/questions/36069731/how-to-unit-test-api-calls-with-mocked-fetch-in-react-native-with-jest
+  global.fetch = jest.fn(() => Promise.resolve());
   const { getByText } = render(<AboutDialog onClose={() => {}} />);
   expect(getByText(/About/i)).toBeTruthy();
 });
