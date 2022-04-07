d9db75f5bf4000b7dfaf28d34c59dcf9938a59ef
Luke Murphey <Luke.Murphey@gmail.com>
2022-04-07 01:29:38 -0400

    Adding ability to toggle local/remote servers

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,28 @@
+/**
+ * The dev server can be changed to use a publicly available version of TextCritical or a local
+ * version of a server. Make a file at webpack.config.json with the following to have the frontend
+ * talk to a local server:
+ * 
+ *      {
+ *        "endpoint": "local"
+ *      }
+ */
+
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+
+let config = null;
+
+try {
+  config = require('./webpack.config.json');
+  console.log("Loaded config from the local config");
+} catch (error) {
+  // Default to using a public server
+  config = {
+    "endpoint": "remote"
+  };
+  console.log("No local config found");
+}
 
 // Use this endpoint if you are connecting to the localhost running the textcritical backend
 const localEndpoint = {
@@ -15,7 +38,7 @@ const remoteEndpoint = {
 };
 
 // Use either localEndpoint or remoteEndpoint
-const endpoint = remoteEndpoint;
+const endpoint = config.endpoint === 'local' ? localEndpoint : remoteEndpoint;
 
 module.exports = merge(common, {
   mode: 'development',
