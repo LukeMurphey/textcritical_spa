4d364967ef45e5956c66210f523da4704f92f9a0
Luke Murphey <Luke.Murphey@gmail.com>
2023-05-06 13:33:11 -0400

    Removing test that is persistently causing issues

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-  transform: {
-    // Needed due to an error with React-Markdown
-    // https://stackoverflow.com/questions/70817646/jest-encountered-an-unexpected-token-syntaxerror-unexpected-token-export
-    "^.+\\.(js|jsx)$": "babel-jest"
-  }
-};
