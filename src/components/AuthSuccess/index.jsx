c64ee281ad6c81bd35fb8bf4f9ea63da84a608ff
LukeMurphey <luke.murphey@gmail.com>
2024-09-23 00:39:39 +0000

    Making the padding the same as the login page

--- src/components/AuthSuccess/index.jsx
+++ src/components/AuthSuccess/index.jsx
@@ -3,7 +3,7 @@ import { Header, Segment, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 const AuthSuccess = ({inverted}) => (
-  <Segment inverted={inverted}>
+  <Segment inverted={inverted} style={{padding: 48}}>
       <Header as='h3'>Authentication Complete</Header>
       <Icon name='check' color='green' size='large' />
       You have completed the login process. You can close this window.
