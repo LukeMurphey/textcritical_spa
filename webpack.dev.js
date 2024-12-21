6bc198e16b070c8f5ca1f8fee94fba4f464b10fd
lmurphey <lmurphey@splunk.com>
2024-12-21 21:12:53 +0000

    Added logging of which endpoint is being used

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,7 +15,6 @@ let config = null;
 
 try {
   config = require('./webpack.config.json');
-  console.log("Loaded config from the local config");
 } catch (error) {
   // Default to using a public server
   config = {
@@ -39,6 +38,8 @@ const remoteEndpoint = {
 
 // Use either localEndpoint or remoteEndpoint
 const endpoint = config.endpoint === 'local' ? localEndpoint : remoteEndpoint;
+
+console.info(`Using endpoint: ${endpoint.target}`);
 
 module.exports = merge(common, {
   mode: 'development',
